bac5e77ec4f9e7989fc01fa1f66927668eb6f716
wonho <dnjsgh04@gmail.com>
2023-04-12 16:47:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gittest1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.gittest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
